a0b2730a7402b39a1747361ca4173df0a59f94ad
atteez <atte-98@windowslive.com>
2023-11-22 16:45:37 +0200

    tuotteiden näyttäminen joulukortit

--- src/pages/Joulukortit.js
+++ src/pages/Joulukortit.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function Home() {
+const URL = 'http://localhost:3001/'
+
+export default function GetKortit() {
+  const [products, setProducts] = useState([])
+  const [category, setCategory] = useState("Joulukortit")
+
+  const params = {
+    category: category
+  }
+
+  useEffect(() => {
+    axios.get(URL + 'products', {params: params})
+    .then(resp => setProducts(resp.data))
+    .catch(error => console.log(error.message))
+  
+ 
+  }, [category])
+  
   return (
     <div className='content'>
+      <div className='product-container'>
+        {products.map(p => (
+          <div key={p.id} className='product-item'>
+            <img src={`${URL}${p.imageUrl}`} alt="Kuva" className='product-image' />
+            <p className='product-name'>{p.productName}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
+
 }
