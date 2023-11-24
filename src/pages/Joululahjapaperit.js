b8deb34cad1bf8b62c6e2eb254fa4a47c69eee7c
JussiMo <n2moju00@students.oamk.fi>
2023-11-24 17:58:24 +0200

    asettelua

--- src/pages/Joululahjapaperit.js
+++ src/pages/Joululahjapaperit.js
@@ -3,6 +3,38 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='content'>
+      <div className='otsikko'>
+        <h1>Alueen otsikko</h1>
+      </div>
+      <div className='products'>
+        <h2>tuotteet</h2>
+        <div className='product-container'>
+          <div className='product'>
+            <h3>tuote 1</h3>
+            <ul>
+              <li>
+                <p>kuvaus 1</p>
+              </li>
+            </ul>
+          </div>
+          <div className='product'>
+            <h3>tuote 2</h3>
+            <ul>
+              <li>
+                <p>kuvaus 2</p>
+              </li>
+            </ul>
+          </div>
+          <div className='product'>
+            <h3>tuote 3</h3>
+            <ul>
+              <li>
+                <p>kuvaus 3</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Tekijat.js
+++ src/pages/Tekijat.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='content'>
+      <h1>Tekijät</h1>
     </div>
   )
 }
