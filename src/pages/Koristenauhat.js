bd9f0451d4b4a26030014823b7c549042192207f
atteez <atte-98@windowslive.com>
2023-11-28 13:59:58 +0200

    päivitettyversio

--- src/pages/Koristenauhat.js
+++ src/pages/Koristenauhat.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function Home() {
+const URL = 'http://localhost:3001/'
+
+export default function GetKortit() {
+  const [products, setProducts] = useState([])
+  const [category, setCategory] = useState("Koristenauhat")
+
+  const params = {
+    category: category
+  }
+
+  useEffect(() => {
+    axios.get(URL + 'products', {params: params})
+    .then(resp => setProducts(resp.data))
+    .catch(error => console.log(error.message))
+  
+ 
+  }, [category])
+  
   return (
+    
     <div className='content'>
+      <h2 className='otsikko'>Koristenauhat</h2>
+      <div className='product-container'>
+        {products.map(p => (
+          <div key={p.id} className='product-item'>
+            <img src={`${URL}${p.imageUrl}`} alt="Kuva" className='product-image' />
+            <h3 className='product-name'>{p.productName}</h3>
+            <p>{p.price + '€'}</p>
+            <button type="button" class="btn btn-secondary">Lisää ostoskoriin</button>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
+
 }
